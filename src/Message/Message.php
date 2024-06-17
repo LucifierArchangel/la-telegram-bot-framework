e612558c6384e9ec03007afee1af3e8e94cc6b89
Chantico <131584072+KianticO@users.noreply.github.com>
2024-06-17 20:51:15 +0300

    refactor(message): optimize checkData method

--- src/Message/Message.php
+++ src/Message/Message.php
@@ -1,91 +1,92 @@
 <?php
 
-    namespace Lucifier\Framework\Message;
+namespace Lucifier\Framework\Message;
+
+/**
+ * Simple message builder with simple template engine
+ */
+abstract class Message
+{
+    /**
+     * @var bool message preview
+     */
+    protected bool $preview = false;
 
     /**
-     * Simple message builder with simple template engine
+     * @var string message type for view
      */
-    abstract class Message {
-        /**
-         * @var bool message preview
-         */
-        protected $preview = false;
+    protected string $type = 'send';
 
-        /**
-         * @var string message type for view
-         */
-        protected $type="send";
+    /**
+     * @var array array of field's names for template engine
+     */
+    protected $fields = [];
 
-        /**
-         * @var array array of field's names for template engine
-         */
-        protected $fields = array();
+    /**
+     * @var string message template
+     */
+    protected $template = '';
 
-        /**
-         * @var string message template
-         */
-        protected $template = "";
+    /**
+     * Validates data against specified fields
+     *
+     * @param array $data Key-value array of data for message templating
+     * @return bool Returns true if all specified fields are present in the data, false otherwise
+     */
+    private function checkData(array $data): bool
+    {
+        $missingFields = array_diff($this->fields, array_keys($data));
 
-        /**
-         * Validating data against specified fields
-         * @param $data array key:value array of data for message templating
-         * @return bool       validation result
-         */
-        private function checkData(array $data): bool {
-            foreach ($this->fields as $field) {
-                if (!array_key_exists($field, $data)) {
-                    return false;
-                }
-            }
+        return empty($missingFields);
+    }
 
-            return true;
-        }
+    /**
+     * Get message type
+     *
+     * @return mixed|string
+     */
+    public function getType(): mixed
+    {
+        return $this->type;
+    }
 
-        /**
-         * Get message type
-         *
-         * @return mixed|string
-         */
-        public function getType(): mixed {
-            return $this->type;
-        }
+    /**
+     * Get message preview
+     *
+     * @return bool
+     */
+    public function getPreview(): bool
+    {
+        return $this->preview;
+    }
 
-        /**
-         * Get message preview
-         *
-         * @return bool
-         */
-        public function getPreview(): bool {
-            return $this->preview;
-        }
+    /**
+     * Compile template
+     * @param $data array key:value array of data for message templating
+     * @return void
+     */
+    private function compile(array $data): void
+    {
+        preg_match_all('~\{{\s*(.+?)\s*\}}~is', $this->template, $matches, PREG_SET_ORDER);
 
-        /**
-         * Compile template
-         * @param $data array key:value array of data for message templating
-         * @return void
-         */
-        private function compile(array $data): void {
-            preg_match_all('~\{{\s*(.+?)\s*\}}~is', $this->template, $matches, PREG_SET_ORDER);
-
-            foreach ($matches as $match) {
-                $this->template = str_replace($match[0], $data[$match[1]], $this->template);
-            }
-        }
-
-        /**
-         * Data validation and template compilation
-         * @param $data array key:value array of data for message templating
-         * @return string     compilation result
-         */
-        public function run(array $data=array()): string {
-            if ($this->checkData($data)) {
-                $this->compile($data);
-
-                return $this->template;
-            }
-
-            return "";
+        foreach ($matches as $match) {
+            $this->template = str_replace($match[0], $data[$match[1]], $this->template);
         }
     }
 
-?>
+    /**
+     * Data validation and template compilation
+     * @param $data array key:value array of data for message templating
+     * @return string     compilation result
+     */
+    public function run(array $data = []): string
+    {
+        if ($this->checkData($data)) {
+            $this->compile($data);
+
+            return $this->template;
+        }
+
+        return '';
+    }
+}
