b578ef1cae55032cdb863bcf11174c526ddbb53e
Chantico <131584072+KianticO@users.noreply.github.com>
2024-11-25 21:39:25 +0300

    fix: Fixed work with the database, crutches if necessary....

--- src/Core/Database/IDatabase.php
+++ src/Core/Database/IDatabase.php
@@ -1,35 +1,35 @@
 <?php
 
-    namespace Lucifier\Framework\Core\Database;
+namespace Lucifier\Framework\Core\Database;
 
-    use mysqli;
+use mysqli;
+use mysqli_result;
 
-    interface IDatabase {
-        public static function getInstance(): IDatabase;
+interface IDatabase
+{
+    public static function getInstance(): IDatabase;
 
-        public function openNew(string $server, string $user, string $password, string $name, ?string $port = null): void;
+    public function openNew(string $server, string $user, string $password, string $name, ?string $port = null): void;
 
-        public function getConnection(): mixed;
+    public function getConnection(): mysqli;
 
-        public function query(string $sql, array $substitutions=[]): mixed;
+    public function query(string $sql, array $substitutions = []): mysqli_result|array|bool;
 
-        public function substitute(string $sql, array $substitutions): array|string|null;
+    public function substitute(string $sql, array $substitutions): string;
 
-        public function escape(mixed $value): array|int|string;
+    public function escape(mixed $value): string;
 
-        public function getCell(string $sql, array $substitutions=[]): mixed;
+    public function getCell(string $sql, array $substitutions = []): mixed;
 
-        public function getRow(string $sql, array $substitutions=[]): mixed;
+    public function getRow(string $sql, array $substitutions = []): array;
 
-        public function getColumn(string $sql, bool $makeHash=false, array $substitutions=[]): array;
+    public function getColumn(string $sql, bool $makeHash = false, array $substitutions = []): array;
 
-        public function getTable(string $sql, bool|string $keyCol=false, array $substitutions=[]): array;
+    public function getTable(string $sql, bool|string $keyCol = false, array $substitutions = []): array;
 
-        public function writeRow(string $tableName, array $data, mixed $unique_key=false, string|bool $mode=false): int|string;
+    public function writeRow(string $tableName, array $data, bool|int|array $unique_key = false, string|bool $mode = false): int|bool;
 
-        public function convertKeyValuePairForWriting(mixed $key, mixed $value): string;
+    public function convertKeyValuePairForWriting(mixed $key, mixed $value): string;
 
-        public function close(?mysqli $connect): void;
-    }
-
-?>
+    public function close(): void;
+}
